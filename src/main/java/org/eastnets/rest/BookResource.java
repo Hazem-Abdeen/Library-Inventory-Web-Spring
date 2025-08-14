0030eddfe12e6576e310b45e00eb0e494587bb9b
Hazem Abdeen <hazem.abdeen39@gmail.com>
2025-08-14 12:04:44 +0300

    run spring

--- src/main/java/org/eastnets/rest/BookResource.java
+++ src/main/java/org/eastnets/rest/BookResource.java
@@ -1,33 +1,4 @@
 package org.eastnets.rest;
 
-import org.eastnets.model.Book;
-import org.eastnets.service.BookService;
-import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.MediaType;
-import java.util.List;
-
-@Path("/books")
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
 public class BookResource {
-
-    private final BookService bookService = new BookService();
-
-    @GET
-    public List<Book> getAllBooks() {
-        return bookService.getAllBooks();
-    }
-
-    @POST
-    public void addBook(Book book) {
-        System.out.println("POST /api/books was called");
-        System.out.println("Received book: " + book.getTitle());
-        bookService.addBook(book);
-    }
-
-    @DELETE
-    @Path("/{id}")
-    public void deleteBook(@PathParam("id") int id) {
-        bookService.deleteBook(id);
-    }
 }
